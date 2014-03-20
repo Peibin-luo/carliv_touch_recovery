112dff9c159d0b0138ee34cdb2bb3bd287b700a9
carliv <liviucar@yahoo.com>
2014-03-20 11:42:43 +0200

    revert non-working pigz support

--- Android.mk
+++ Android.mk
@@ -193,5 +193,4 @@ include $(commands_recovery_local_path)/updater/Android.mk
 include $(commands_recovery_local_path)/applypatch/Android.mk
 include $(commands_recovery_local_path)/utilities/Android.mk
 include $(commands_recovery_local_path)/su/Android.mk
-include $(commands_recovery_local_path)/pigz/Android.mk
 commands_recovery_local_path :=
